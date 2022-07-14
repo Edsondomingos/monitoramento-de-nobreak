fc48d7a3465ab579a1a0e7067165069cb36ae1bd
Edson Domingos <edomingoswork@gmail.com>
2022-07-14 11:23:42 -0300

    Atualização tela ligar desligar

--- src/components/LigaDesligar.js
+++ src/components/LigaDesligar.js
@@ -1,18 +1,55 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { Text, View, StyleSheet, Switch } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Switch,
+  TouchableHighlight,
+} from 'react-native';
 
 export default function LigaDesligar() {
+  const [liga, setLigar] = useState(false);
 
-  const [liga, setLigar] = useState(false)
-
-  function switchLigar(){
-    setLigar(!liga)
+  function switchLigar() {
+    setLigar(!liga);
   }
 
   return (
-    <View>
-      <Switch value={liga} onValueChange={() => switchLigar()}/>
+    <View style={styles.container}>
+      <TouchableHighlight 
+          style={liga ? styles.botao : styles.botaoD}
+          onPress={switchLigar}
+        >
+        <Text style={styles.txtBt}>{liga ? 'LIGADO' : 'DESLIGADO'}</Text>
+      </TouchableHighlight>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  botao: {
+    width: 200,
+    height: 200,
+    borderWidth: 1,
+    borderRadius: '50%',
+    backgroundColor: '#7f7'
+  },
+  botaoD: {
+    width: 200,
+    height: 200,
+    borderWidth: 1,
+    borderRadius: '50%',
+    backgroundColor: '#f33'
+  },
+  txtBt:{
+    margin: 'auto',
+    fontSize: 30,
+    fontWeight: 'bolder'
+  },
+});
