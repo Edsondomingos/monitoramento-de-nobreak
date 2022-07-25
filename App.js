9d9f6235519ed98cbcff5b9c18d063911a50499e
Edson Domingos da Silva <e.domingos@academico.ifrn.edu.br>
2022-07-25 10:20:25 -0300

    tela monitoramento para styled-components

--- App.js
+++ App.js
@@ -6,10 +6,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import Login from './src/components/Login';
 import Monitoramento from './src/components/Monitoramento';
 import Mudar from './src/components/Mudar';
+import Teste from './assets/styledComponents/Teste'
 import Cadastro from './src/components/Login';
 
 export default function App() {
   return (
-      <Cadastro />
+      <Monitoramento />
   )
 }
