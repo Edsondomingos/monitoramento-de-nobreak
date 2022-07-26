6f198f27fd6600ce39788d9fe7100f93d4b14b63
Fabiana de Souza Silva <fabiana.souza@academico.ifrn.edu.br>
2022-07-26 16:52:40 -0300

    Menu editado

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,17 @@
 import * as React from "react";
-import { Container,Botao, TextoBtn } from "../../assets/styledComponents/Components";
+import { Container,Botao, TextoBtn, MiniContainer } from "../../assets/styledComponents/Components";
 
 export default (props) => {
   return (
     <Container >
-      <Container  style={{flexDirection:"row"}}>
+      <MiniContainer  style={{flexDirection:"row"}}>
         <Botao onPress={() => { props.navigation.navigate("EditarPerfil"); }} >
           <TextoBtn>Editar Perfil</TextoBtn>
         </Botao>
         <Botao onPress={() => { props.navigation.navigate("Cadastrar_Nobreak"); }}>
           <TextoBtn>Cadastrar</TextoBtn>
         </Botao>
-      </Container>
+      </MiniContainer>
         <Botao onPress={() => { props.navigation.navigate("Monitoramento"); }}>
           <TextoBtn>Monitoramento</TextoBtn>
         </Botao>
