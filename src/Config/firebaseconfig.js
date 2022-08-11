1249a0074e31462ad9605c01e574515d7db77dd3
Fabiana de Souza Silva <fabiana.souza@academico.ifrn.edu.br>
2022-08-11 14:44:03 -0300

    'Resolvendo'

---
+++ src/Config/firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ5QsSE3ikoavnaqP93qJE_KQyYF8Xliw",
+  authDomain: "nobreak-b714a.firebaseapp.com",
+  projectId: "nobreak-b714a",
+  storageBucket: "nobreak-b714a.appspot.com",
+  messagingSenderId: "987534923711",
+  appId: "1:987534923711:web:f80c12033fc46ebd50730a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/components/Cadastrar_Nobreak.js
+++ src/components/Cadastrar_Nobreak.js
@@ -1,16 +1,45 @@
 import * as React from "react";
 import { ImageBackground } from "react-native";
+//import { Text, View, TextInput, Button } from "react-native";
 import { Container, Titulo, Entrada, Botao, TextoBtn } from "../../assets/styledComponents/Components";
+import { useState } from "react";
+import { app } from '../Config/firebaseconfig';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
-export default (props) => {
+export default () => {
+
+  const [codigo, setCodigo] = useState('')
+  const [mac, setMac] = useState('')
+  
+  function cadastro() {
+    const auth = getAuth(app);
+    createUserWithEmailAndPassword(auth, codigo, mac)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        alert("Cadastrou");
+        setCodigo('');
+        setMac('');
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        //menssagem de alerta
+        alert(errorMessage);
+        // ..
+      });
+  }
+
   return (
     <Container>
       <ImageBackground source={require('../../img/iot.jpg')} style={{ width: 100, height: 100 }} >
       </ImageBackground>
       <Titulo>Cadastrar Nobreak</Titulo>
-      <Entrada placeholder="Codigo/Modelo" />
-      <Entrada placeholder="Endereço MAC" />
-      <Botao testID='btn' onPress={() => props.navigation.navigate('Menu')} >
+      <Entrada placeholder="Codigo/Modelo" onChangeText={setCodigo} />
+      <Entrada placeholder="Endereço MAC" onChangeText={setMac} />
+      
+      <Botao testID='btn' onPress={cadastro} >
         <TextoBtn>Cadastrar</TextoBtn>
       </Botao>
 
