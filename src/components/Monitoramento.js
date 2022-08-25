52aebacf703bb89377f74b0ab4ea96009f8ea9ee
Edson Domingos <edomingoswork@gmail.com>
2022-08-25 11:33:15 -0300

    Update Monitoramento.js

--- src/components/Monitoramento.js
+++ src/components/Monitoramento.js
@@ -1,21 +1,20 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { Container, Titulo, TextoComum, Botao, Circulo, LigaDesliga, TextoBtn } from '../../assets/styledComponents/Components'
-import { App, Red, onoff } from './TesteMQTT'
-// import Status from './statusEnergia'
+import { App} from './TesteMQTT'
 
 export default function Monitoramento(props) {
 
   const [liga, setLigar] = useState(false);
   const [nome, setNome] = useState('Nome do Nobreak')
  
-  setNome(props.route.params.cod)
+  
 
   return (
     <Container>
       
   
-      <Titulo>{nome}</Titulo>  
+      <Titulo>{props.route.params.cod}</Titulo>
 
       <App />     
 
