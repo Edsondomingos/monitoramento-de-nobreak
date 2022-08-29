4b62844e614ed5ad8839f6a0d38593458d8b4f3f
Edson Domingos <edomingoswork@gmail.com>
2022-08-29 16:50:17 -0300

    Update Cadastro.js

--- monitoramento-de-nobreak-firebase9/src/components/Cadastro.js
+++ monitoramento-de-nobreak-firebase9/src/components/Cadastro.js
@@ -4,62 +4,27 @@ import { Container, Titulo, TextoBtn, Botao, Entrada } from '../../assets/styled
 import { useState } from "react";
 import { app } from '../Config/firebaseconfig';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { db } from '../Config/firebaseconfig';
+import firebase from '../Config/firebaseconfig';
 import { addDoc, collection } from 'firebase/firestore';
-//import * as yup from "yup";
 
 export default (props) => {
 
-/*  const login = (values) => console.log(values);
-
-  const validarlogin = yup .object().shape({
-    email: yup.string().email().required(),
-    senha: yup.string().min(8).required(),
-  }); */
-
     const [nome, setNome] = useState('')
-    const [cpf, setCPF] = useState('')
     const [email, setEmail] = useState('')
-    const [senha, setSenha] = useState('')
     const [telefone, setTelefone] = useState('')
+    const [cpf, setCPF] = useState('')
+    const [senha, setSenha] = useState('')
     
-    function cadastrarusu() {
-      const auth = getAuth(db);
-      createUserWithEmailAndPassword(auth, email, senha)
-        .then((userCredential) => {
-          // Signed in
-          const user = userCredential.user;
-          //alert("Cadastrou");
-          setEmail('');
-          setSenha('');
-          alert("Cadastrado com sucesso");
-          props.navigation.navigate("Login");
-          // ...
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          //menssagem de alerta
-          alert(errorMessage);
-          // ..
-        });
-        //setDoc(doc(db, "contatos", "LA"), {
-        //await addDoc(collection(db, "contatos"), {
-        addDoc(collection(db, 'usuarios'), {
-            nome: nome,
-            senha: senha,
-            email: email,
-            telefone: telefone,
-            cpf: cpf
-          }).then(() => {
-            //alert("Cadastrado com sucesso");
-            //props.navigation.navigate("Login");
-          });
-          //setEmail('');
-          //setSenha('');
-          //setCPF('');
-          //setNome('');
-          //setTelefone('');
+    async function cadastrarUsuario() {
+        await firebase.auth().createUserWithEmailAndPassword(email, senha)
+            .then(database => {
+                const uid = database.user.uid;
+                const users = firebase.firestore().collection('users');
+                users.doc(uid).set({
+                    name: nome, email: email, telefone: telefone, cpf: cpf, senha: senha
+                });
+            });
+        props.navigation.navigate("Login")
     }
 
     return (
@@ -86,7 +51,7 @@ export default (props) => {
             />
             <Botao
                 testID='btnCriar'
-                onPress={cadastrarusu}
+                onPress={cadastrarUsuario}
             >
                 <TextoBtn>Criar Conta</TextoBtn>
             </Botao>
